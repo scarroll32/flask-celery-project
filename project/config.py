e8100d268dce75f38765d9585a833795091e88e6
Sean Carroll <sean@immersive.ch>
2022-03-05 10:58:40 +0100

    Step: Application Factory (fixed)

--- project/config.py
+++ project/config.py
@@ -10,7 +10,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3")
 
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
-    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis:://127.0.0.1:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""

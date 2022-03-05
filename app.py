0a77ba3f20f5c593e961a6691f1c4249d79a60ac
Sean Carroll <sean@immersive.ch>
2022-03-05 15:36:29 +0100

    Step: Setting Up Auto-reload

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ celery = ext_celery.celery
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+@app.cli.command("celery_worker")
+def celery_worker():
+    from watchgod import run_process
+    import subprocess
+
+    def run_worker():
+        subprocess.call(
+            ["celery", "-A", "app.celery", "worker", "--loglevel=info"]
+        )
+
+    run_process("./project", run_worker)
